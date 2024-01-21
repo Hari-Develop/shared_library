13ca26e5757f411f6f1d42421ced5e67c95211ea
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2024-01-21 11:02:57 +0530

    commit

--- var/CI.groovy
+++ var/CI.groovy
@@ -6,19 +6,16 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building the application...'
-
             }
         }
         stage('Test') {
             steps {
                 echo 'Testing the application...'
-                // Add test commands here (e.g., unit tests)
             }
         }
         stage('Deploy') {
             steps {
                 echo 'Deploying the application...'
-                // Add deployment commands here
             }
         }
     }
